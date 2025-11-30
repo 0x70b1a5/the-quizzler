1d1bdbec95735b5158fc6a5acf456164efc2f12a
Tobias Merkle <tgamerkle@gmail.com>
2025-11-30 14:19:33 -0500

    uuid

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,22 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "AgentKit demo",
-  description: "Demo of ChatKit with hosted workflow",
+  title: "The Quizzler",
+  description: "Quiz generator powered by ChatKit",
 };
+
+// Polyfill for crypto.randomUUID in non-secure contexts (HTTP)
+const cryptoPolyfill = `
+if (typeof crypto !== 'undefined' && typeof crypto.randomUUID !== 'function') {
+  crypto.randomUUID = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0;
+      var v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+}
+`;
 
 export default function RootLayout({
   children,
@@ -15,6 +28,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Polyfill crypto.randomUUID for HTTP contexts */}
+        <script dangerouslySetInnerHTML={{ __html: cryptoPolyfill }} />
         <Script
           src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
           strategy="beforeInteractive"
